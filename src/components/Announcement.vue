57896b98fd634e22ac2a40c157a4146094f251bf
yvnan2.19 <yveskouadio111@yahoo.fr>
2020-02-04 22:25:02 +0000

    addin

---
+++ src/components/Announcement.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="alert alert-info">{{announcement}} </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            announcement: 'Le site est en cours de construction'
+        }
+    }
+}
+</script>
